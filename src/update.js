c80d55c01982473fca9168ec8892e309b9ee1884
ManojGowda89 <mail@manojgowda.in>
2026-07-29 14:30:24 +0530

    fix

--- src/update.js
+++ src/update.js
@@ -1,16 +1,25 @@
 import * as Updates from "expo-updates";
 import {consoleApp} from "./handlelogs";
+import {updatePersistentNotification} from "./bgn";
 
 export async function checkForOTAUpdates() {
   // Development Build / Expo Go
   if (__DEV__) {
     consoleApp("Skipping OTA check (development mode)");
+    updatePersistentNotification({
+      title: "OTA Update Check",
+      body: "Skipping OTA check (development mode)"
+    })
     return;
   }
 
   // Development Client
   if (Updates.isEmbeddedLaunch) {
     consoleApp("Embedded launch");
+    updatePersistentNotification({
+      title: "OTA Update Check",
+      body: "Embedded launch"
+    });
   }
 
   try {
@@ -25,16 +34,32 @@ export async function checkForOTAUpdates() {
 
     if (update.isAvailable) {
       consoleApp("New OTA update available");
+      updatePersistentNotification({
+        title: "OTA Update Available",
+        body: "Downloading update..."
+      });
 
       await Updates.fetchUpdateAsync();
 
       consoleApp("Reloading...");
+      updatePersistentNotification({
+        title: "OTA Update Downloaded",
+        body: "Reloading app..."
+      });
 
       await Updates.reloadAsync();
     } else {
       consoleApp("Already up to date");
+      updatePersistentNotification({
+        title: "OTA Update Check",
+        body: "Already up to date"
+      });
     }
   } catch (e) {
+    updatePersistentNotification({
+      title: "OTA Update Error",
+      body: e.message || "Unknown error"
+    });
     consoleApp("OTA Update Error: " + e);
   }
 }
